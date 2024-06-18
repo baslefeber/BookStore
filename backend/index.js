33db853cdc496f815be393a4f75da3382ecd4035
baslefeber <buzzlowmusic@gmail.com>
2024-06-18 13:37:37 +0300

    dependencies again

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,22 @@
 import express from 'express';
-import { PORT, mongoDBURL } from './config.js'
+import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 
 const app = express();
 
-
 app.get('/', (req, res) => {
-    console.log(req)
-    return res.status(234).send('YOOOOOOOO')
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(req);
+    return res.status(234).send('YOOOOOOOO');
 });
 
 mongoose
     .connect(mongoDBURL)
     .then(() => {
         console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
     })
     .catch((error) => {
-        console.log(error);
+        console.log('Error connecting to MongoDB:', error);
     });
